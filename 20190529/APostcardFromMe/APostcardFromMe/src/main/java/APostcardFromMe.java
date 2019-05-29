1279f7ac9af4e2f6816359738c1ac261084b972f
harineem1 <41168415+harineem1@users.noreply.github.com>
2019-05-28 20:21:05 -0400

    unit1

---
+++ 20190529/APostcardFromMe/APostcardFromMe/src/main/java/APostcardFromMe.java
@@ -0,0 +1,16 @@
+public class APostcardFromMe {
+    public static void main(String[] args) {
+        System.out.println("*------------------------------------------------------------------*\n" +
+                "| John Smith \t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n" +
+                "| 123 Main Street\t\t\t\t\t\t\t\t\t\t\t\t\t|\n" +
+                "| Hometown, MN 55555 \t\t\t\t\t\t\t\t\t\t\t\t|\n" +
+                "|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n" +
+                "|\t\t\t\t\t\tJane Doe\t\t\t\t\t\t\t\t\t|\n" +
+                "|\t\t\t\t\t\t345 Mockingbird Ln.\t\t\t\t\t\t\t|\n" +
+                "|\t\t\t\t\t\tSmalltown, OR 99999\t\t\t\t\t\t\t|\n" +
+                "|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n" +
+                "|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n" +
+                "*------------------------------------------------------------------**");
+
+         }
+}
---
+++ 20190529/MarvelTable/src/test/java/mytest1.java
@@ -0,0 +1,18 @@
+public class mytest1 {
+        public static void main(String[] args) {
+            System.out.println("-------------------------------------------------------------\n" +
+                    "|\tHero\t\t\t|\tAffiliation\t\t|\tPower\t\t\t|\n"  +
+                    "-------------------------------------------------------------\n" +
+                    "|\tCaptain America\t|\tThe Avengers\t|\tPeak Human\t\t|\n" +
+                    "-------------------------------------------------------------\n" +
+                    "|\tWolverine\t\t|\tThe X-Men\t\t|\tHealing/Claws\t|\n" +
+                    "-------------------------------------------------------------\n" +
+                    "|\tCyclops\t\t\t|\tThe X-Men\t\t|\tEye Beam\t\t|\n" +
+                    "-------------------------------------------------------------\n" +
+                    "|\tDomino\t\t\t|\tX-Force\t\t\t|\tLuck\t\t\t|\n" +
+                    "-------------------------------------------------------------");
+        }
+    }
+
+
+
